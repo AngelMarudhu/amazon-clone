eacbae2409ea4d346d64b7adfde138021096c498
Marudhu <angeldeveloper09@gmail.com>
2022-01-29 00:52:37 +0530

    amazon version 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Header";
+import Homepage from "./Homepage";
+import Checkout from "./Checkout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/checkout"
+            element={
+              <>
+                <Header /> <Checkout />
+              </>
+            }
+          />
+          <Route path="/login" element={<h1>LoginPage</h1>} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header /> <Homepage />
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
